49bfc9a1fc614dccbf48628866eaeca406434497
grekiki <gregor1234567890@gmail.com>
2019-06-07 15:09:00 +0200

    Naredimo možnost shranjevanja sprememb

--- model.py
+++ model.py
@@ -1,18 +1,56 @@
+import datetime
+import json
 class uporabnik:
     def __init__(this,ime,geslo,podjetje,spremembe):
         this.ime=ime
         this.geslo=geslo
         this.podjetje=podjetje
         this.spremembe=spremembe
+        this.delta=this.parse(spremembe)
+        print(this.ime+" "+str(this.delta))
+    def parse(this,spremembe):
+        seznam=[]
+        oklepaj=-1
+        vejica=-1
+        zaklepaj=-1
+        for i in range(0,len(spremembe)):
+            if(spremembe[i]=="("):
+                oklepaj=i
+            elif(spremembe[i]==","):
+                vejica=i
+            elif(spremembe[i]==")"):
+                zaklepaj=i
+                ok=(oklepaj>0 and oklepaj<zaklepaj and oklepaj<vejica and vejica<zaklepaj)
+                #print(ok)
+                #print(spremembe[1+oklepaj:vejica]+" "+spremembe[1+vejica:zaklepaj])
+                ls=spremembe[1+oklepaj:vejica].split(".")
+                seznam.append(((int(ls[0]),int(ls[1]),int(ls[2])),int(spremembe[1+vejica:zaklepaj])))
+        return seznam
+    def sprememba(this,datum,stevilo):
+        for i in range(0,len(delta)):
+            t=this.delta[i]
+            if(t[0]==datum):
+                this.delta[i]=(datum,stevilo)
+                updateStr()
+                return
+        this.delta.append((datum,stevilo))
+        this.updateStr()
+    def updateStr(this):
+        ans="{"
+        for t in this.delta:
+            ans+="("+t[0][0]+"."+t[0][1]+"."+t[0][2]+","+t[1]+"),"
+        ans+="}"
+        this.spremembe=ans
 
 class Model:
     def __init__(this):
         this.readSaveFile()
     def readSaveFile(this):
-        print("read")
+        #print("read")
         file=open("podatki.txt","r")
         this.users=[]
         for line in file:
+            #print(line)
             arr=line.split()
             ime=arr[0]
             geslo=arr[1]
@@ -41,3 +79,11 @@ class Model:
         this.users.append(uporabnik(ime,geslo,podjetje,"{}"))
         this.writeSaveFile()
         return True
+    def dodajSpremembo(this,ime,datum,stevilo):
+        for user in this.users:
+            if(user.ime==ime):
+                user.sprememba(datum,stevilo)
+                writeSaveFile()
+                return True
+        print("Nekaj je narobe v posodobitvah")
+        return False
